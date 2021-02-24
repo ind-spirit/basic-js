7ef34102db281d130d8ff29bbf188632c409fcba
ind-spirit <error156@yandex.ru>
2021-02-24 23:05:29 +0700

    dream team completed

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+    let i = 0,
+        res = [],
+        n = 0;
+    if (!Array.isArray(arr)) {
+        return false;
+    } else {
+        for (i = 0; i < arr.length; i++) {
+            if (typeof arr[i] === 'string') {
+                arr[i] = arr[i].split('').filter((item) => item != ' ');
+                res[n] = arr[i][0];
+                n++;
+            }
+
+        }
+    }
+    res = res.join('').toUpperCase().split('').sort().join('');
+    return res;
 };
