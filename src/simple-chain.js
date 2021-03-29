3684cacbce92e617e854f4abab416a211cc244a2
ind-spirit <error156@yandex.ru>
2021-03-29 21:24:06 +0700

    feat: make simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
+
+
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+    chain: [],
+
+
+    getLength() {
+        return this.chain.length;
+    },
+    addLink(value) {
+        if (value !== undefined) {
+            this.chain.push(`( ${value} )`);
+        } else {
+            this.chain.push(`( )`);
+        }
+        return this;
+    },
+    removeLink(position) {
+        if (typeof position !== "number" || this.chain[position - 1] === undefined) {
+            throw new Error();
+        } else {
+            this.chain.splice(position - 1, 1);
+            return this;
+        }
+        
+    },
+    reverseChain() {
+        this.chain.reverse();
+        return this;
+    },
+    finishChain() {
+        return this.chain.join('~~');
+    }
 };
 
+//console.log(chainMaker.chain);
+//console.log(chainMaker.addLink('abc'));
+//console.log(chainMaker.finishChain());
+//console.log(chainMaker.addLink('abc').finishChain())
+//console.log(chainMaker.addLink(1).addLink().addLink(2).reverseChain().addLink(3).finishChain());
+//chainMaker.addLink('abc').finishChain();
 module.exports = chainMaker;
